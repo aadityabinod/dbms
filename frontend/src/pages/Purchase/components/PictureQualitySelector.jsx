df09130d8250799b66f14374d5626363d5b089b1
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2025-02-23 13:29:59 +0545

    picture quality

--- frontend/src/pages/Purchase/components/PictureQualitySelector.jsx
+++ frontend/src/pages/Purchase/components/PictureQualitySelector.jsx
@@ -122,7 +122,7 @@ export const PictureQualitySelector = ({
           {`${show.hall_name} (${show.show_type})`}
           <div className="form-showtimes">{options}</div>
         </div>
-        <p className="form-show-price">{`BDT ${show.price_per_seat}TK`}</p>
+        <p className="form-show-price">{`NPR ${show.price_per_seat} RS`}</p>
       </div>
     );
   });
